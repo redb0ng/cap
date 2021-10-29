12c633d6aa68e6ed5bb168507a9db7065df12a7f
KANGBONG <cww21@naver.com>
2021-10-29 19:05:51 +0900

    sencond

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //database: 'mongodb://localhost:27017/meanauth',
+  database:
+    "mongodb+srv://kangbong:hS9pvesvEp6xGQuE@cluster0.zgzi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secret: "yoursupersecret",
+  rounds: 10,
+};
